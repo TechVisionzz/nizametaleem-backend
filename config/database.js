654c57b249fb37cc6f7842609be8aec6501bef15
[Ahtash Naeem] <[ahtasham067@gmail.com]>
2022-04-08 14:13:47 +0500

    orgsettings changes

--- config/database.js
+++ config/database.js
@@ -1,33 +1,11 @@
 const path = require('path');
 
-
-if (process.env.DATABASE_CLIENT && process.env.DATABASE_CLIENT !== 'sqlite' ) {
-  module.exports = ({ env }) => ({
+module.exports = ({ env }) => ({
+  connection: {
+    client: 'sqlite',
     connection: {
-      client: env('DATABASE_CLIENT', 'mysql'),
-      connection: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 3306),
-        database: env('DATABASE_NAME', 'strapi'),
-        user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
-        ssl: {
-          rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
-        },
-      },
-      debug: false,
+      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
     },
-  });
-} else {
-  module.exports = ({ env }) => ({
-    connection: {
-      client: 'sqlite',
-      connection: {
-        filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
-      },
-      useNullAsDefault: true,
-    },
-  });
-  
-}
-
+    useNullAsDefault: true,
+  },
+});
